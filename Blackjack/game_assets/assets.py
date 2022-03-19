20ed3dab7e4ad47c6d3d9eb223d56d84395f8c30
Robert Vári <mail.robertvari@gmail.com>
2022-03-19 17:10:33 +0100

    update

--- Blackjack/game_assets/assets.py
+++ Blackjack/game_assets/assets.py
@@ -24,6 +24,24 @@ class Deck:
     def create(self):
         self._cards.clear()
 
+        cards = [
+            ["2", 2],
+            ["3", 3],
+            ["4", 4],
+            ["5", 5],
+            ["6", 6],
+            ["7", 7],
+            ["8", 8],
+            ["9", 9],
+            ["10", 10],
+            ["King", 10],
+            ["Queen", 10],
+            ["Jack", 10],
+            ["Ace", 11]
+        ]
+
+        names = ["Heart", "Club", "Diamond", "Spade"]
+
     def __repr__(self):
         return f"{self._cards}"
 
