e46ed9ddc8f023a5926256b730ad34f4729c2ba1
Robert Vári <mail.robertvari@gmail.com>
2022-03-20 16:55:56 +0100

    Project init

---
+++ BattleOfClasses/game_assets/characters.py
@@ -0,0 +1,15 @@
+class CharacterBase:
+    def __init__(self):
+
+
+
+class Player(CharacterBase):
+    pass
+
+
+class Enemy(CharacterBase):
+    pass
+
+
+if __name__ == '__main__':
+    enemy = Enemy()
